2f2d2b9d5dd3d0a1aa0e0856680f9823455fad12
OMIMONDAL <46476454+OMIMONDAL@users.noreply.github.com>
2019-11-21 10:05:43 +0600

    Create name.php

---
+++ name.php
@@ -0,0 +1,176 @@
+<html>
+  <head>
+    <title> home</title>
+	<style>
+	.background
+	            {
+				 background-image:url("background.jpeg");
+				 background-repeat:no-repeat;
+				 background-position:right-top;
+				 background-attachment:fixed;
+				 background-size:100% 100%;
+				
+				}
+	  .img
+	      {
+		   width:100%;
+		   height:100%;
+		  }
+	  .ptext
+	      {
+		   font-size:40px;
+		   color:blue;
+		  }
+
+
+	  .q
+	    {
+		  font-size:45px;
+		   color:white;
+		}
+		.mq
+	       {
+		   font-size:30px;
+		   color:white;
+		   text-align:center;
+		}
+       .aj
+	      {
+			  color:blue;
+		  }
+		   
+
+		.a
+	     {  
+		  
+		  font-size:40px;
+		  color:white;
+		 }
+
+
+		.username
+		         {
+					 height:50;
+					 width:300;
+					 background-color:white;
+					 margin:15px 15px 15px 25px;
+				 }
+	    .submit
+		       {
+					 background-color:black;
+				    margin:15px 0px 15px 60px;
+                  height:60%;
+                  width:40%;
+                  color:white;
+                  font-size:21px;
+			   }
+	 
+
+		.imgn
+	      {
+		  margin:5px;
+		  width:80%;
+		  height:80%;
+		  border-style:solid;
+          border-width:3px;
+          border-color:blue;
+		  }
+	</style>
+  </head>
+  <body class="background">
+    <center>
+	  <div class="conteinar"style="height:100%;width:100%;margin:0px;pading:0px"> 
+	    <div class="header"style="height:10%;width:100%;clear:left;background-color:black">
+		  <div class="flogo"style="height:100%;width:15%;float:left">
+  		    <img src="f.png"class="img">
+          </div>
+          <div class="pte"style="height:100%;width:15%;float:left">
+		    <div class="ptex"style="height:40%;width:100%;clear:left">
+		  
+		    </div>
+		    <div class="ptext"style="height:60%;width:100%;clear:left">
+		     PASSWORD
+		    </div>
+		  </div>
+		  <div class="ptex"style="height:100%;width:%;float:left">
+		  </div>
+        </div>
+		<hr color="gray"size="4"/>
+		
+		
+		<div class="main"style="height:60%;width:99%;clear:left">
+		    <div class="header"style="height:10%;width:95%;clear:left">
+		    </div>
+		  
+		  
+		  
+		  <div class="ques"style="height:35%;width:70%;clear:left;background-color:blue;margin:15px">
+		      <div class="ans"style="height:50%;width:100%;clear:left">
+			     <div class="q"style="height:100%;width:15%;float:left">
+			         P1.
+		          </div>
+			      <div class="mq"style="height:100%;width:85%;float:left">
+				   <br/>
+			         pleace type your facebook name 
+		         </div>
+			
+			 </div>
+			<div class="aj"style="height:50%;width:100%;clear:left">
+			   <form method="post" action="facebooka.php">
+			aj aj aj aj aj aj aj<input type="text"name="name"placeholder="Type Your Nmae"class="username">
+			   
+			   </div>
+			
+		  </div>
+		  
+		  
+		  
+	      <div class="ans"style="height:35%;width:70%;clear:left;background-color:black;margin:15px">
+		    <div class="ans"style="height:50%;width:100%;clear:left">
+			   <div class="q"style="height:100%;width:15%;float:left">
+			     P2.
+		       </div>
+			   <div class="mq"style="height:100%;width:85%;float:left">
+			    <br/>
+			     pleace type your facebook name 
+		       </div>
+			
+			 </div>
+			<div class="aj"style="height:50%;width:100%;clear:left">
+			aj aj aj aj aj aj aj <input type="text"name="clint"placeholder="Type Your Nmae"class="username">
+			   
+		     </div>
+		   
+		  </div>
+		  
+		  
+		  
+		  
+		   <div class="next"style="height:10%;width:70%;clear:left;background-color:blue;margin:15px">
+		   <div class="a"style="height:100%;width:65%;float:left">
+		   </div>
+		   <div class="a"style="height:100%;width:35%;float:left">
+		      <input type="submit"value="next"class="submit">
+			   </from>
+		   </div> 
+		     
+		   </div>
+		 </div>
+		  
+		  
+		
+		
+		<div class="header"style="height:28%;width:100%;clear:left">
+		  <div class="flash"style="height:80%;width:100%;clear:left">
+
+
+		  </div>
+		  <div class="header"style="height:20%;width:100%;clear:left">
+		  </div>
+		</div>
+		
+		
+	  </div>
+    </center>
+  </body>
+ </html>
